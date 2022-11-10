8eb393472551d7b9929ac84c7c15055867d439e9
Dan Lozynsky <danlozynsky@gmail.com>
2022-11-10 22:50:23 +0000

    Renaming files

--- src/main/java/com/example/individualprojectrepo/LifeSimulatorController.java
+++ src/main/java/com/example/individualprojectrepo/LifeSimulatorController.java
@@ -3,7 +3,7 @@ package com.example.individualprojectrepo;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
 
-public class HelloController {
+public class LifeSimulatorController {
     @FXML
     private Label welcomeText;
 
